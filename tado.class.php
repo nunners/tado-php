4eb5353cbad6b1f38a55fe420b6a83cdeb4f897a
nunners <3448152+nunners@users.noreply.github.com>
2023-08-31 18:54:16 +0100

    Create tado.class.php

---
+++ tado.class.php
@@ -0,0 +1,192 @@
+<?php
+
+class Tado {
+
+        //https://shkspr.mobi/blog/2019/02/tado-api-guide-updated-for-2019/
+
+        public $auth_url = "https://auth.tado.com/oauth/token";
+        public $api_url_v1 = "https://my.tado.com/api/v1/";
+        public $api_url_v2 = "https://my.tado.com/api/v2/";
+        private $username = "";
+        private $password = "";
+        private $client_secret = "wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc";
+        private $access_token = false;
+        private $refresh_token = false;
+        public $me = [];
+        public $home_id = false;
+        public $zones = array();
+        public $error = false;
+
+        function __construct() {
+
+                        $this->authenticate();
+                        if (!$this->access_token) {
+                                return $this->error;
+                        }
+                        $this->getMe();
+
+                        if (!$this->home_id) {
+                                return $this->error;
+                        }
+
+                        $this->getZones();
+                        return ($this);
+
+        }
+
+        private function authenticate() {
+
+                        $client_id = "tado-web-app";
+                        $grant_type = "password";
+                        $scope = "home.user";
+
+                        $headers = array(
+                                        'Content-Type: application/x-www-form-urlencoded',
+                        );
+
+                        $ch = curl_init();
+
+                        $data = "client_id=tado-web-app&grant_type=password&scope=home.user&client_secret=" . $this->client_secret . "&username=" . $this->username ."&password=" . $this->password;
+
+                        curl_setopt($ch, CURLOPT_URL, $this->auth_url);
+                        curl_setopt($ch, CURLOPT_POST, true);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+                        $results = curl_exec($ch);
+
+                        curl_close($ch);
+
+                        $json_results = json_decode($results);
+
+                        if (isset($json_results->access_token)) {
+
+                                $this->access_token = $json_results->access_token;
+                                $this->refresh_token = $json_results->refresh_token;
+                                return $this;
+                        } else {
+                                $this->error = curl_error($ch);
+                                return false;
+                        }
+
+
+        }
+
+        private function api($endpoint, $api_url) {
+
+                        $api_url = $api_url.$endpoint;
+
+                        $headers = array(
+                                        'Content-Type: application/x-www-form-urlencoded',
+                                        'Authorization: Bearer ' . $this->access_token
+                        );
+
+                        $ch = curl_init();
+
+                        curl_setopt($ch, CURLOPT_URL, $api_url);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+                        $results = curl_exec($ch);
+                        $json_results = json_decode($results);
+
+                        curl_close($ch);
+
+                        return $json_results;
+
+        }
+
+        private function getMe() {
+
+                        $results = $this->api("me", $this->api_url_v1);
+
+                        if (!isset($results->homeId)) {
+                                $this->error = "No home id";
+                                return false;
+                        }
+                        $this->me = $results;
+                        $this->home_id = $results->homeId;
+
+                        return $this;
+
+        }
+
+        public function getZones() {
+
+                        $results = $this->api("homes/" . $this->home_id . "/zones", $this->api_url_v2);
+                        foreach ($results as $result) {
+
+                                        if ($result->type=="HEATING") {
+                                                        $this->zones[$result->name] = array("id"=>$result->id, "type"=>"Heating");
+                                        } else {
+                                                        $this->zones[$result->name] = array("id"=>$result->id, "type"=>"Other");
+                                        }
+
+                        }
+
+                        return $this;
+
+        }
+
+                public function listZones() {
+
+                        return $this->zones;
+
+                }
+
+        public function getZoneIdFromName ($zone_name) {
+
+                        if (isset($this->zones[$zone_name])) {
+
+                                        return $this->zones[$zone_name]["id"];
+
+                        } else {
+
+                                        return false;
+                        }
+
+        }
+
+        public function getZoneStateFromId ($zone_id) {
+
+                        return $this->api("homes/" . $this->home_id . "/zones/" . $zone_id . "/state", $this->api_url_v2);
+
+        }
+
+        public function getZoneStateFromName ($zone_name) {
+
+                        $zone_id = $this->getZoneIdFromName($zone_name);
+                        return $this->getZoneStateFromId ($zone_id);
+
+        }
+
+        public function getZoneTemperature($zone_identifier) {
+
+                        $zone_state = $this->getZoneStateFromName($zone_name);
+                        return $zone_state->sensorDataPoints->insideTemperature->celsius;
+
+        }
+
+        public function getZoneHumidity($zone_identifier) {
+
+                        $zone_state = $this->getZoneStateFromName($zone_name);
+                        return $zone_state->sensorDataPoints->humidity->percentage;
+
+        }
+
+        public function getZoneHeating_target($zone_identifier) {
+
+                        $zone_state = $this->getZoneStateFromName($zone_name);
+                        if ($zone_state->setting->power=="OFF") {
+                                        return 0;
+                        } else {
+                                return $zone_state->setting->temperature;
+                        }
+        }
+
+}
+
+?>
